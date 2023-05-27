ad5b1bd8b61f6a51171c6a47fd59ecfb2eb3e6a4
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-05-28 00:29:46 +0530

    Create concatenation.js

---
+++ DataTypes/Strings/concatenation.js
@@ -0,0 +1,7 @@
+//Method 1 using '+' Operator
+let s1 = "I am Manoj" , s2 = "Welcome to my System";
+console.log(s1+" "+s2); //I am Manoj Welcome to my System
+
+//Method 2 using TEMPLATE LITERALS
+let s3 = `Hello All, ${s1} and ${s2}`;
+console.log(s3);    //Hello All, I am Manoj and Welcome to my System
