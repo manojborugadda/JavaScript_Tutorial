f328b69c873d90e161bc37d849daf08df103f529
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-05-29 13:45:53 +0530

    Create Using Backslash character.js

---
+++ DataTypes/Strings/Using Backslash character.js
@@ -0,0 +1,9 @@
+let str = "This is a long string and we cannot \
+write this in one single long line \
+so we can use this backslash character \
+so that it will be easy for other Devs to read easily";
+console.log(str);
+
+
+
+// we use the this backslash character( \ ) for creating long strings
