11cc420ede7e1ae3acc86e12a41ca4a2d484519f
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-05-30 15:19:45 +0530

    Update comparing Strings.js

--- DataTypes/Strings/comparing Strings.js
+++ DataTypes/Strings/comparing Strings.js
@@ -9,3 +9,8 @@ const s1 = new String("javascript");
 const s2 = new String("javascript");
 console.log("without converting into primitive string -----> result is : " ,s1 === s2); //false
 console.log("after converting into primitive string using toString method ---> result is : " ,s1.toString() === s2.toString()); //true
+
+/**
+ If you do need to compare a primitive string to a string object,
+ we can use the toString() method to explicitly convert the string object to a primitive string before comparing.
+*/
