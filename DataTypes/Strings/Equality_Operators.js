01d6b485d868535e138339a138b09e131e3d5e35
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-05-31 00:42:24 +0530

    Create Equality_Operators.js

---
+++ DataTypes/Strings/Equality_Operators.js
@@ -0,0 +1,10 @@
+//EQUALITY " == " and STRICT EQUALITY " === "
+
+// “==” operator compares only the values of two strings for equality, but it does not compare their types.
+
+// On the other hand, the “===” operator compares the values of two strings and their data types
+
+console.log( “10” == 10 ) //true
+
+console.log("10" === 10); // false
+
