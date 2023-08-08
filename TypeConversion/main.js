9d90f9e0b747c195ae5937310a576f370a60b801
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-08-08 23:47:37 +0530

    Update main.js

--- TypeConversion/main.js
+++ TypeConversion/main.js
@@ -1,4 +1,4 @@
-/** In programming, type conversion is the process of converting DATA of one TYPE to another. For example: converting String data to Number.
+/** In programming, type conversion is the process of converting the DATA of one TYPE to another. For example: converting String data to Number.
  There are two types of type conversion in JavaScript.
  Implicit Conversion - automatic type conversion
  Explicit Conversion - manual type conversion
