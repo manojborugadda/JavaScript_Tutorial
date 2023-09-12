0ae8927e1451ab0e69d428071382bd4c6beeeb48
Manoj Borugadda <borugadda.manoj@gmail.com>
2023-09-13 00:09:19 +0530

    Update main.js

--- Functions/function Expressions/main.js
+++ Functions/function Expressions/main.js
@@ -1,7 +1,10 @@
-// example 1:
+// Example 1:
 
-let x = function (num) { 
-  return num * num 
-};
-console.log(x(4));
+let x = function(num) {
+    return num*num;
+}
+console.log(x(6)); //36
 
+//Example 2: Reassigning the function expression
+let y = x(7);
+console.log(y); //49
